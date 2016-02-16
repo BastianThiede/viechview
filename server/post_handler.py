90c15ffaaec623e974066daec7ee90006d4e1433
k4FF33P0TT <2thiede@informatik.uni-hamburg.de>
2016-02-17 00:46:00 +0100

    slightly changed serv.py and shamelessly renamed it

---
+++ server/post_handler.py
@@ -0,0 +1,32 @@
+import os
+import posixpath
+import BaseHTTPServer
+import simplejson
+from BaseHTTPServer import BaseHTTPRequestHandler
+import cgi
+
+class PostHandler(BaseHTTPRequestHandler):
+        def _set_headers(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+        def _handle_img(self,img):
+            pass
+
+	def do_POST(self):
+	    self._set_headers()
+	    print "in post method"
+	    self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+	    self.send_response(200)
+	    self.end_headers()
+
+	    data = simplejson.loads(self.data_string)
+            self._handle_img(self.data['img'])
+
+
+if __name__ == '__main__':
+    from BaseHTTPServer import HTTPServer
+    server = HTTPServer(('localhost', 8080), PostHandler)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
